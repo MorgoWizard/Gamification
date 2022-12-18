cef697cbf363f1eb66eb07ea7d63b40f0dbd4ede
Alexander Salkov <zzeronnx@gmail.com>
2022-12-18 15:43:13 +0300

    Firas stage done

--- Gamification/Assets/Scripts/PlayerController/Scripts/Components/GroundCheck.cs
+++ Gamification/Assets/Scripts/PlayerController/Scripts/Components/GroundCheck.cs
@@ -13,7 +13,7 @@ public class GroundCheck : MonoBehaviour
     /// </summary>
     public event System.Action Grounded;
 
-    const float OriginOffset = .001f;
+    const float OriginOffset = 0.1f;
     Vector3 RaycastOrigin => transform.position + Vector3.up * OriginOffset;
     float RaycastDistance => distanceThreshold + OriginOffset;
 
