d6261874aaef827471150962b616a8fe034cdf01
thefireKS <KS29092003@gmail.com>
2023-01-03 22:50:45 +0300

    new microfixes of UI

--- Gamification/Assets/Scripts/CamControl.cs
+++ Gamification/Assets/Scripts/CamControl.cs
@@ -2,26 +2,24 @@ using UnityEngine;
 
 public class CamControl : MonoBehaviour
 {
+    [SerializeField] private float minClamp, maxClamp;
+    
     public float mouseSensitivity = 100f;
 
-    private Transform _cam;
-
-    private float _xRotation;
+    private float _yRotation;
 
     private void OnEnable()
     {
-        _cam = this.gameObject.transform;
         Cursor.lockState = CursorLockMode.Locked;
     }
     
     private void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-        _xRotation -= mouseY;
-        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
         
-        _cam.Rotate(Vector3.up * mouseX);
+        _yRotation += mouseX;
+        _yRotation = Mathf.Clamp(_yRotation, minClamp, maxClamp);
+
+        transform.localRotation = Quaternion.Euler(0f, _yRotation, 0f);
     }
 }
