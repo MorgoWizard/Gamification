33ed85e064c9260ca4f6d0bc45d56787754f2a64
Nikita <druzhbinn@gmail.com>
2023-01-19 20:20:57 +0300

    RangeTest Kirill check

--- Gamification/Assets/Scripts/ScalerWithWheel.cs
+++ Gamification/Assets/Scripts/ScalerWithWheel.cs
@@ -4,7 +4,11 @@ public class ScalerWithWheel : MonoBehaviour
 {
     [SerializeField] private float minScale, maxScale;
     private float _currentScale;
-
+    
+    [SerializeField] private RectTransform contentContainer;
+    [SerializeField] private float contentWidthMin, contentWidthMax, contentHeightMin, contentHeightMax;
+    private float _currentContentWidth, _currentContentHeight;
+    
     private RectTransform _rectTransform;
 
     private void Start()
@@ -16,10 +20,13 @@ public class ScalerWithWheel : MonoBehaviour
 
     private void Update()
     {
+        _currentContentWidth = contentWidthMin + (_currentScale - minScale) / (maxScale - minScale)* (contentWidthMax - contentWidthMin);
+        _currentContentHeight = contentHeightMin + (_currentScale - minScale) / (maxScale - minScale) * (contentHeightMax - contentHeightMin);
         float wheelInput = Input.GetAxis("Mouse ScrollWheel");
         _currentScale += wheelInput;
         if (_currentScale > maxScale) _currentScale = maxScale;
         if (_currentScale < minScale) _currentScale = minScale;
         _rectTransform.localScale = new Vector2(_currentScale, _currentScale);
+        contentContainer.sizeDelta = new Vector2(_currentContentWidth, _currentContentHeight);
     }
 }
